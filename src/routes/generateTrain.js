725bba8863c0118f1b06d6686407b73095a37355
Soner <80179760+sonerady@users.noreply.github.com>
2025-03-04 13:08:35 +0300

    fixed

--- src/routes/generateTrain.js
+++ src/routes/generateTrain.js
@@ -651,7 +651,7 @@ router.post("/generateTrain", upload.array("files", 50), async (req, res) => {
 
             // Model create
             const model = await replicate.models.create(
-              "nodselemen",
+              "snda21",
               repoName,
               {
                 visibility: "private",
@@ -665,7 +665,7 @@ router.post("/generateTrain", upload.array("files", 50), async (req, res) => {
               "flux-dev-lora-trainer",
               "e440909d3512c31646ee2e0c7d6f6f4923224863a6a10c494606e79fb5844497",
               {
-                destination: `nodselemen/${repoName}`,
+                destination: `snda21/${repoName}`,
                 input: {
                   steps: 1000,
                   lora_rank: 20,
